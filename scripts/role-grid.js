aa1a4f13e4c5a3366bbb682b4eb14bd467649599
Alex Bokii <vanilinka33@gmail.com>
2017-06-08 22:05:34 +0100

    Added function to select a neighbouring gridcell

--- scripts/role-grid.js
+++ scripts/role-grid.js
@@ -13,9 +13,30 @@ gridlistCell.forEach(gc => gc.addEventListener('focus', function(ev) {
 
 document.addEventListener('keydown', function(ev) {
     if(ev.keyCode >= 37 || ev.keyCode <= 40 && document.activeElement.classList.contains('gridlist-cell')) {
-        selectNeighbouringGridListCell(ev.keyCode);
+        findNeighbouringGridListCell(ev.keyCode);
     }
 });
+
+function findNeighbouringGridListCell(keyCode) {
+    const firstGridlistCell = gridlistCell[0];
+    const lastGridlistCell = gridlistCell[gridlistCell.length - 1];
+    let activeElement = document.activeElement;
+
+    if(keyCode === 37 || keyCode === 38) {
+        // left
+        if(activeElement === firstGridlistCell) {
+            selectGridListCell(lastGridlistCell);
+        }
+
+    }
+    else if(keyCode === 39 || keyCode === 40) {
+        // right
+        if(activeElement === lastGridlistCell) {
+            selectGridListCell(firstGridlistCell)
+        }
+
+    }
+}
 
 function selectGridListCell(el) {
     gridlistCell.forEach(gc => gc.addEventListener('click', function(ev) {
@@ -24,15 +45,4 @@ function selectGridListCell(el) {
 
     el.setAttribute('tabindex', '0');
     el.focus();
-}
-
-function selectNeighbouringGridListCell(keyCode) {
-    let activeElement = document.activeElement;
-    if(keyCode === 37 && keyCode === 38) {
-        // up
-
-    }
-    else if(keyCode === 39 && keyCode === 40) {
-        // down
-    }
 }
